lm-tools/your-work-search

Add pg databese - use bookshelf as orm - create Jobs model - create jobs migration - configure travis

knexfile.js
@@ -1,0 +1,21 @@
+// Update with your config settings.
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      database: 'your_work_search',
+    },
+  },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      database: 'your_work_search_test',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
+};

lm-tools/hello-world

knexfile.js
@@ -1,0 +1,21 @@
+// Update with your config settings.
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      database: 'hello_world',
+    },
+  },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || {
+      database: 'hello_world_test',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
+};