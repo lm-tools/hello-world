lm-tools/your-work-search

Add support for mounting the app under a path
Setting the EXPRESS_BASE_PATH environment variable will mount the app
within that variable.

Feature tests are now run twice, once without EXPRESS_BASE_PATH set,
and once with it set.

app/app.js
@@ -18,13 +18,18 @@ app.engine('mustache', cons.hogan);
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
 
+// run the whole application in a directory
+const basePath = app.locals.basePath = process.env.EXPRESS_BASE_PATH || '';
+const assetPath = `${basePath}/`;
+
 // Middleware to set default layouts.
 // This must be done per request (and not via app.locals) as the Consolidate.js
 // renderer mutates locals.partials :(
 app.use((req, res, next) => {
   // eslint-disable-next-line no-param-reassign
   res.locals = {
-    assetPath: '/',
+    assetPath,
+    basePath,
     partials: {
       layout: 'layouts/main',
       govukTemplate:
@@ -45,12 +50,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'dist', 'public')));
-app.use(express.static(path.join(__dirname, '..',
+app.use(assetPath, express.static(path.join(__dirname, '..', 'dist', 'public')));
+app.use(assetPath, express.static(path.join(__dirname, '..',
   'vendor', 'govuk_template_mustache_inheritance', 'assets')));
 
-app.use('/', dashboard);
-app.use('/:accountId/jobs', jobs);
+app.use(`${basePath}/`, dashboard);
+app.use(`${basePath}/:accountId/jobs`, jobs);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

lm-tools/hello-world

(cherry picked from commit 4b6132318674eb0dffad5079da118d903a4860af)

app.js
@@ -16,12 +16,18 @@ app.engine('mustache', cons.hogan);
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
 
+// run the whole application in a directory
+const basePath = app.locals.basePath = process.env.EXPRESS_BASE_PATH || '';
+const assetPath = `${basePath}/`;
+
 // Middleware to set default layouts.
 // This must be done per request (and not via app.locals) as the Consolidate.js
 // renderer mutates locals.partials :(
 app.use((req, res, next) => {
   /* eslint no-param-reassign: "off" */
   res.locals = {
+    assetPath,
+    basePath,
     partials: {
       layout: 'layouts/main',
       govukTemplate: '../vendor/govuk_template_mustache_inheritance/views/layouts/govuk_template',
@@ -40,12 +46,13 @@ app.use(logger.init(app.get('env')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist/public')));
-app.use(express.static(path.join(__dirname,
+
+app.use(assetPath, express.static(path.join(__dirname, 'dist', 'public')));
+app.use(assetPath, express.static(path.join(__dirname,
   'vendor', 'govuk_template_mustache_inheritance', 'assets')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(`${basePath}/`, routes);
+app.use(`${basePath}/users`, users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {