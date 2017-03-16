509db826509f241545edba9f8ac0092434202b58
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-03-16 14:36:44 +0000

    Handle 404 and 500 errors

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const indexController = require('./controllers/index-controller');
 const usersController = require('./controllers/users-controller');
 const i18n = require('./middleware/i18n');
+const errorHandler = require('./middleware/error-handler');
 const healthCheckController = require('./controllers/health-check-controller');
 
 const app = express();
@@ -72,29 +73,6 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {},
-  });
-});
-
+errorHandler(app);
 
 module.exports = app;
